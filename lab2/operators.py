8f15ad1b53be4fe278716f13d50948e5f2405362
Aida <aida.krtva@gmail.com>
2024-01-20 20:26:48 +0600

    lab2

---
+++ lab2/operators.py
@@ -0,0 +1,12 @@
+print (10 * 5) #1
+print (10 / 2) #2
+
+fruits  = ["apple", "banana"]           #3
+if "apple" in fruits:
+    print ("Yes, apple is a fruit!")
+
+if ( 5 != 10):                          #4
+    print ("5 and 10 is not equal")
+
+if 5 == 10 or 4 == 4:
+    print ("At least one of the statements is true") #5
