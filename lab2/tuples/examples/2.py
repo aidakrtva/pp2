cc857742db08afd56c282e596cfbb98a1f37bb26
Aida <aida.krtva@gmail.com>
2024-01-31 20:17:39 +0600

    added examples for tuples

---
+++ lab2/tuples/examples/2.py
@@ -0,0 +1,35 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+print ("......................")
+
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+print ("......................")
+
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+print ("......................")
+
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+print ("......................")
+
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+print ("......................")
+
+
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+print ("......................")
+
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+print ("......................")
---
+++ lab2/tuples/examples/4.py
@@ -0,0 +1,28 @@
+fruits = ("apple", "banana", "cherry")
+print(fruits)
+print ("......................")
+
+
+fruits = ("apple", "banana", "cherry")
+(green, yellow, red) = fruits
+print(green)
+print(yellow)
+print(red)
+print ("......................")
+
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, yellow, *red) = fruits
+print(green)
+print(yellow)
+print(red)
+print ("......................")
+
+
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+(green, *tropic, red) = fruits
+print(green)
+print(tropic)
+print(red)
+print ("......................")
