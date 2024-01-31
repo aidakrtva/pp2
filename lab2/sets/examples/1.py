a6142d59c2a2ffe904475b07ea3e2bad870febac
Aida <aida.krtva@gmail.com>
2024-01-31 20:25:59 +0600

    added exam[les for sets

---
+++ lab2/sets/examples/1.py
@@ -0,0 +1,48 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry", False, True, 0}
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry"}
+print(len(thisset))
+print ("......................")
+
+
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+print(set1)
+print(set2)
+print(set3)
+print ("......................")
+
+
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
+print ("......................")
+
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+print ("......................")
+
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+print ("......................")
---
+++ lab2/sets/examples/2.py
@@ -0,0 +1,66 @@
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+thisset.update(tropical)
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+thisset.update(mylist)
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry"}
+x = thisset.pop()
+print(x)
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry"}
+del thisset
+print(thisset)
+print ("......................")
+
+
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+print ("......................")
